33aa57bf551e8b916c4ac3dcb5c4a36153d13e9f
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-20 13:38:15 +0900

    Update variables.tf

--- modules/aurora/variables.tf
+++ modules/aurora/variables.tf
@@ -1,1 +1,8 @@
-
+variable "name"            { type = string }
+variable "engine"          { type = string default = "aurora-mysql" }
+variable "engine_version"  { type = string }
+variable "vpc_id"          { type = string }
+variable "subnet_ids"      { type = list(string) }
+variable "master_username" { type = string default = "admin" }
+variable "master_password" { type = string default = "sophielog" }
+variable "tags"            { type = map(string) default = {} }

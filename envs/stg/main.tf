d21b9c656697e1a131e29b9e4c78e24e9fb23368
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-20 17:34:53 +0900

    Update main.tf

--- envs/stg/main.tf
+++ envs/stg/main.tf
@@ -1,1 +1,36 @@
-
+# 기본 네임스페이스
+resource "kubectl_manifest" "namespaces" {
+  yaml_body = <<YAML
+apiVersion: v1
+kind: Namespace
+metadata: { name: "kube-system" }
+---
+apiVersion: v1
+kind: Namespace
+metadata: { name: "karpenter" }
+---
+apiVersion: v1
+kind: Namespace
+metadata: { name: "observability" }
+---
+apiVersion: v1
+kind: Namespace
+metadata: { name: "matching" }
+---
+apiVersion: v1
+kind: Namespace
+metadata: { name: "trading" }
+---
+apiVersion: v1
+kind: Namespace
+metadata: { name: "wallet" }
+---
+apiVersion: v1
+kind: Namespace
+metadata: { name: "risk" }
+---
+apiVersion: v1
+kind: Namespace
+metadata: { name: "opencost" }
+YAML
+}

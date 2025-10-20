cbfe868aacb7d5c7e4bd07eb0d7d07e3b671361f
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-20 13:35:56 +0900

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,3 +1,6 @@
-output "karpenter_security_group_id" { value = aws_security_group.karpenter_shared.id }
-output "private_subnet_ids"          { value = var.private_subnet_ids }
-output "public_subnet_ids"           { value = var.public_subnet_ids }
+output "cluster_id"       { value = aws_eks_cluster.this.id }
+output "cluster_name"     { value = aws_eks_cluster.this.name }
+output "cluster_arn"      { value = aws_eks_cluster.this.arn }
+output "cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
+output "cluster_ca"       { value = aws_eks_cluster.this.certificate_authority[0].data }
+output "nodegroup_role_name" { value = aws_iam_role.ng.name }

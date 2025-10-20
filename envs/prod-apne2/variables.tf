d8e2ba5bdf590efa3fc9c0fc35e58d9531ad7fe0
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-20 13:53:24 +0900

    Delete envs/prod-apne2 directory

--- envs/prod-apne2/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region"          { type = string  default = "ap-northeast-2" }
-variable "cluster_name"    { type = string  default = "upx-eks-prod-apne2" }
-variable "vpc_id"          { type = string }
-variable "private_subnet_ids" { type = list(string) }
-variable "public_subnet_ids"  { type = list(string) }
-variable "karpenter_version"  { type = string default = "v0.37.0" }
-variable "kubernetes_version" { type = string default = "1.29" }
-variable "tags"              { type = map(string) default = { "env" = "prod" } }

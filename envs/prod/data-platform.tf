51d18f30ea36f341c6dca4563d18db5308b5ef5c
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-20 14:01:50 +0900

    Create data-platform.tf

---
+++ envs/prod/data-platform.tf
@@ -0,0 +1,32 @@
+# MSK
+module "msk" {
+  source          = "../../modules/msk"
+  cluster_name    = "${var.cluster_name}-msk"
+  region          = var.region
+  vpc_id          = var.vpc_id
+  subnet_ids      = module.network.private_subnet_ids
+  kafka_version   = var.kafka_version
+  brokers_per_az  = var.msk_brokers_per_az
+  sasl_username   = var.msk_sasl_username
+  sasl_password   = var.msk_sasl_password
+  tags            = var.tags
+}
+
+# OpenCost (FinOps)
+module "opencost" {
+  source       = "../../modules/opencost"
+  namespace    = "opencost"
+  cluster_name = var.cluster_name
+  currency     = "USD"
+  tags         = var.tags
+}
+
+# ClickHouse (분석)
+module "clickhouse" {
+  source              = "../../modules/clickhouse"
+  namespace           = var.clickhouse_namespace
+  replicas            = var.clickhouse_replicas
+  storage_size        = var.clickhouse_storage
+  zookeeper_endpoints = []
+  tags                = var.tags
+}

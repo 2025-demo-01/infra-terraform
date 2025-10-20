7ef4b265505af8b58ad134c351364348b4388728
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-20 13:40:43 +0900

    Update main.tf

--- modules/opencost/main.tf
+++ modules/opencost/main.tf
@@ -1,1 +1,11 @@
+resource "helm_release" "opencost" {
+  name             = "opencost"
+  repository       = "https://opencost.github.io/opencost-helm-chart"
+  chart            = "opencost"
+  namespace        = var.namespace
+  create_namespace = true
 
+  set { name = "opencost.productMetricsEnabled", value = "true" }
+  set { name = "opencost.kubeClusterName", value = var.cluster_name }
+  set { name = "opencost.currencyCode", value = var.currency }
+}

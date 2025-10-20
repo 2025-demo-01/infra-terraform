27fd9b5051c2456c99719cc11559d0e63d2b63bb
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-20 14:02:18 +0900

    Create storage-analytics.tf

---
+++ envs/prod/storage-analytics.tf
@@ -0,0 +1,12 @@
+# Aurora (거래/지갑 백엔드 예시)
+module "aurora" {
+  source          = "../../modules/aurora"
+  name            = "upx-aurora-prod"
+  engine          = var.aurora_engine
+  engine_version  = var.aurora_engine_version
+  vpc_id          = var.vpc_id
+  subnet_ids      = module.network.private_subnet_ids
+  master_username = var.aurora_master_user
+  master_password = var.aurora_master_pass
+  tags            = var.tags
+}

8f42280e4b73b718246106a4022c30d516668f72
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-20 13:36:50 +0900

    Update variables.tf

--- modules/msk/variables.tf
+++ modules/msk/variables.tf
@@ -1,1 +1,9 @@
-
+variable "cluster_name"     { type = string }
+variable "region"           { type = string }
+variable "vpc_id"           { type = string }
+variable "subnet_ids"       { type = list(string) }
+variable "kafka_version"    { type = string default = "3.6.0" }
+variable "brokers_per_az"   { type = number default = 2 }
+variable "sasl_username"    { type = string default = "sophielog" }
+variable "sasl_password"    { type = string default = "sophielog" }
+variable "tags"             { type = map(string) default = {} }

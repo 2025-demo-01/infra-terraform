3bbc6c4ac4a4ffa6e9d71ad4412cc0f8d0f0f6a7
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-20 17:38:22 +0900

    Create storage-analytics.tf

---
+++ envs/stg/storage-analytics.tf
@@ -0,0 +1,11 @@
+module "aurora" {
+  source          = "../../modules/aurora"
+  name            = "upx-aurora-stg"
+  engine          = var.aurora_engine
+  engine_version  = var.aurora_engine_version
+  vpc_id          = var.vpc_id
+  subnet_ids      = module.network.private_subnet_ids
+  master_username = var.aurora_master_user
+  master_password = var.aurora_master_pass
+  tags            = var.tags
+}

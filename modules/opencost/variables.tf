ea51f2d8be28686b6b8b4b2a5d2c7909a20821fc
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-21 17:25:10 +0900

    Delete modules/opencost directory

--- modules/opencost/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "namespace"    { type = string }
-variable "cluster_name" { type = string }
-variable "currency"     { type = string default = "USD" }
-variable "tags"         { type = map(string) default = {} }

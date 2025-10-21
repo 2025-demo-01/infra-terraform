c5697988bef0b2e26c850cd92af43cfec2ccf273
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-21 17:24:50 +0900

    Delete modules/network directory

--- modules/network/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "karpenter_security_group_id" { value = aws_security_group.karpenter_shared.id }
-output "private_subnet_ids"          { value = var.private_subnet_ids }
-output "public_subnet_ids"           { value = var.public_subnet_ids }

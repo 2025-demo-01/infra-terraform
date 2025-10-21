558ba3819e3d05545f0657c0eb8786dad13eadc3
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-21 18:40:28 +0900

    Create security_baseline.tf : GuardDuty + Security Hub 표준 자동 활성화
    탐지/표준은  옵션이 아니라 기본값이기 떄문에 계정 만들자마자 자동 활성화시킴

---
+++ envs/prod/security_baseline.tf
@@ -0,0 +1,9 @@
+resource "aws_guardduty_detector" "this" { enable = true }
+
+resource "aws_securityhub_account" "this" {}
+resource "aws_securityhub_standards_subscription" "cis" {
+  standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.4.0"
+}
+resource "aws_securityhub_standards_subscription" "fsbp" {
+  standards_arn = "arn:aws:securityhub:::ruleset/aws-foundational-security-best-practices/v/1.0.0"
+}

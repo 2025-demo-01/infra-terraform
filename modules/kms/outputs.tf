aedd8ff7000f01f4242cd4c8ff11d212a2160db3
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-21 17:25:01 +0900

    Delete modules/kms directory

--- modules/kms/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "kms_key_id"   { value = aws_kms_key.this.id }
-output "kms_key_arn"  { value = aws_kms_key.this.arn }
-output "kms_alias"    { value = aws_kms_alias.this.name }
--- modules/kms/variables.tf
+++
@@ -1,2 +0,0 @@
-variable "alias" { type = string default = "alias/upx-sops" }
-variable "tags"  { type = map(string) default = {} }

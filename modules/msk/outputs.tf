0252b61b31a3decb930bb3b4fbbd58263317fd38
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-21 17:22:23 +0900

    Delete modules/msk directory

--- modules/msk/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "bootstrap_brokers"       { value = aws_msk_cluster.this.bootstrap_brokers }
-output "bootstrap_brokers_sasl"  { value = aws_msk_cluster.this.bootstrap_brokers_sasl_scram }
-output "security_group_id"       { value = aws_security_group.msk.id }
-output "cluster_arn"             { value = aws_msk_cluster.this.arn }

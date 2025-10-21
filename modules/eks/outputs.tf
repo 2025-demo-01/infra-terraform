b3bd3cba137c5326427c5063dbe298b8c5324dd6
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-21 17:20:20 +0900

    Delete modules/eks directory

--- modules/eks/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cluster_id"       { value = aws_eks_cluster.this.id }
-output "cluster_name"     { value = aws_eks_cluster.this.name }
-output "cluster_arn"      { value = aws_eks_cluster.this.arn }
-output "cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
-output "cluster_ca"       { value = aws_eks_cluster.this.certificate_authority[0].data }
-output "nodegroup_role_name" { value = aws_iam_role.ng.name }
-output "oidc_provider_arn"   { value = var.oidc_provider_arn }  

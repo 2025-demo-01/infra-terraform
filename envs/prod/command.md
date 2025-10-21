9b8d97ae2ec4e700f3cd779495b5a4d9e33d5742
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-21 18:37:24 +0900

    Delete envs/prod directory

--- envs/prod/command.md
+++
@@ -1,3 +0,0 @@
-	1.	cd infra-terraform/envs/prod
-	2.	terraform init
-	3.	terraform apply -var-file=terraform.tfvars
--- envs/prod/security.tf
+++
@@ -1,6 +0,0 @@
-# envs/prod/security.tf (예시)
-resource "aws_config_configuration_recorder" "main" {
-  name     = "default"
-  role_arn = aws_iam_role.config.arn
-}
-# + delivery channel, conformance packs 등

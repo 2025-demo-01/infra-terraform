085ab93d7192795d6bf954823d17bd71918041b2
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-21 17:24:38 +0900

    Delete modules/dr directory

--- modules/dr/outputs.tf
+++
@@ -1,2 +0,0 @@
-output "primary_healthcheck_id"   { value = aws_route53_health_check.primary.id }
-output "secondary_healthcheck_id" { value = aws_route53_health_check.secondary.id }
--- modules/dr/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "hosted_zone"      { type = string } # example: "upx.exchange"
-variable "primary_record"   { type = string } # "api.upx.exchange"
-variable "secondary_record" { type = string } # "api-dr.upx.exchange"
-variable "primary_alb_arn"  { type = string }
-variable "secondary_alb_arn"{ type = string }
-variable "hc_primary_path"  { type = string default = "/healthz" }
-variable "hc_secondary_path"{ type = string default = "/healthz" }
-variable "tags"             { type = map(string) default = {} }

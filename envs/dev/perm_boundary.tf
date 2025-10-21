b05c75f215b55937cee5e95265737c879dcbe261
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-21 18:46:38 +0900

    Create perm_boundary.tf : Engineer Role 생성 시 boundary 적용하는 방식

---
+++ envs/dev/perm_boundary.tf
@@ -0,0 +1,15 @@
+module "perm_boundary" {
+  source        = "../../modules/perm-boundary"
+  name          = "${var.name_prefix}-boundary"
+  allowed_tags  = { env = "dev" }
+}
+
+resource "aws_iam_role" "devops" {
+  name               = "${var.name_prefix}-devops"
+  assume_role_policy = data.aws_iam_policy_document.devops_assume.json
+  permissions_boundary = module.perm_boundary.policy_arn
+  tags = local.tags
+}
+data "aws_iam_policy_document" "devops_assume" {
+  statement { actions=["sts:AssumeRole"], principals{ type="AWS", identifiers=["*"] } }
+}

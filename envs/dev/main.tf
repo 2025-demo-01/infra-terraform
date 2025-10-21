1b6c8c308947b38c3cf14af2714b56db9bca741d
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-21 18:09:33 +0900

    Update main.tf : 앱/플랫폼이 느슨 결합 꿀
    IaC Outputs → SSM → External Secrets로 흘려보냅니다.

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -114,3 +114,11 @@ module "ecr" {
   tags         = local.tags
 }
 
+resource "aws_ssm_parameter" "p" {
+  for_each = var.kv
+  name  = "${var.path_prefix}/${each.key}"
+  type  = "String"
+  value = each.value
+}
+
+

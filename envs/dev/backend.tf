e3f1b15ffd722e06cefd890b5d628e3a28b0c91a
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-21 17:15:09 +0900

    Delete envs/dev directory

--- envs/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "upx-tfstate-sophielog"
-    key            = "eks/dev/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "upx-tflock-sophielog"
-  }
-}
--- envs/dev/network.tf
+++
@@ -1,9 +0,0 @@
-module "network" {
-  source              = "../../modules/network"
-  name                = "upx-dev"
-  region              = var.region
-  vpc_id              = var.vpc_id
-  private_subnet_ids  = var.private_subnet_ids
-  public_subnet_ids   = var.public_subnet_ids
-  tags                = var.tags
-}

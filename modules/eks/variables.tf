779946a01795f898a90bef7d847296f7f5866a62
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-23 13:41:20 +0900

    Update variables.tf : 변수 추가
    Karpenter 사용하는 경우엔 NodePool/EC2NodeClass에 라벨/테인트/CPU옵션 태깅 정책만 문서화

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -5,3 +5,6 @@ variable "private_subnet_ids" { type = list(string) }
 variable "public_subnet_ids"  { type = list(string) }
 variable "node_instance_types" { type = list(string) }
 variable "tags" { type = map(string), default = {} }
+variable "private_subnets" { type = list(string) }
+variable "worker_ami_id"   { type = string }
+
